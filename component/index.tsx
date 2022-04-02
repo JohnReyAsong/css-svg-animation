a451bfdd39f273651d3eddc765308427584a0720
JohnReyAsong <ajohnrey15@gmail.com>
2022-04-02 20:20:15 +0800

    progress

---
+++ component/index.tsx
@@ -0,0 +1,4 @@
+export { default as ManInPhone } from "./ManInPhone";
+export { default as WomenClock } from "./WomenClock";
+export { default as HappyMusic } from "./HappyMusic";
+export { default as AirSupport } from "./AirSupport";
